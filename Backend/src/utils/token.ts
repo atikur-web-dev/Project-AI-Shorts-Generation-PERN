41fbec8373180999a26497b229c72e5e8d30871d
Atikur Rahman <atikurrahman160313@gmail.com>
2026-06-14 22:38:01 +0600

    feat(auth): implement access and refresh token generation and verification

---
+++ Backend/src/utils/token.ts
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import { config } from "../config/index.js";
+import crypto from "crypto";
+import type { AuthPayload } from "../types/auth.types.js";
+
+// Access token generator 
+export const generateAccessToken = (userID: string): string => {
+  return jwt.sign(
+    { userID },
+    config.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+};
+
+// Refresh token generator
+export const generateRefreshToken = (): string => {
+    return crypto.pseudoRandomBytes(64).toString("hex");
+}
+
+// Refresh token hashed
+export const hastToken = (token: string) : string => {
+    return crypto.createHash("sha256").update(token).digest("hex");
+}
+
+// Access token verify
+export const verifyAccessToken = (token: string) : AuthPayload | null => {
+    try {
+        return jwt.verify(token, config.JWT_SECRET) as AuthPayload;
+    } catch (error) {
+        return null
+    }
+}
